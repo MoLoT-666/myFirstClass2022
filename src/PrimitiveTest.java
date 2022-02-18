96eef46b525d09ed879bbeb1e2478c8e2a5c3700
Deskind <desk1nd88@gmail.com>
2022-02-18 23:00:25 +0300

    Add reference/primitive example

---
+++ src/PrimitiveTest.java
@@ -0,0 +1,14 @@
+/**
+ * @author d_shilko
+ *
+ * Created: 18.02.2022
+ */ /*https://javarush.ru/quests/lectures/questsyntax.level02.lecture02*/
+class PrimitiveTest {
+
+    Integer a;
+    int b;
+
+    Boolean aBoolean;
+    boolean bBoolean;
+
+}
